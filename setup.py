b80dbb6772a4b3c2d3e0b5eb0b70f9122437d1d1
Kai97009 <kai97009@gmail.com>
2017-08-14 17:28:15 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='singletonmixin',
-      version='1.0',
+      version='2.0',
       license='MIT License',
       packages=find_packages(),
 )
