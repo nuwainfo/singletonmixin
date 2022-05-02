b8c38b01886bdd0d6f0213131b58b8af6f8e8066
Jason2592 <formosa104@yahoo.com.tw>
2022-05-02 15:27:55 +0800

    修改版本號

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ requires = [
     'six',
 ]
 
-setup(name='singletonmixin',
-      version='2.0',
+setup(name='singletonmixin-python3',
+      version='2.0.1',
       license='MIT License',
       install_requires=requires,
       packages=find_packages(),
