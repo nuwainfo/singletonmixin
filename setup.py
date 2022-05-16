4fb7abaf5981a135acbc47113193fc28de94e80d
Jason2592 <formosa104@yahoo.com.tw>
2022-05-16 15:18:26 +0800

    修改 package 名稱, 將 Branch 拿掉

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requires = [
     'six',
 ]
 
-setup(name='singletonmixin-python3',
+setup(name='singletonmixin',
       version='2.0.1',
       license='MIT License',
       install_requires=requires,
